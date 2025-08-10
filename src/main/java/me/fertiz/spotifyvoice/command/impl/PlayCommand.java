471e38996d516c0f077e911f936384e97c6e00c6
fertiz <fertiz.it@gmail.com>
2025-08-10 22:07:51 +0200

    remove pointless Optional

--- src/main/java/me/fertiz/spotifyvoice/command/impl/PlayCommand.java
+++ src/main/java/me/fertiz/spotifyvoice/command/impl/PlayCommand.java
@@ -14,33 +14,30 @@ public class PlayCommand implements VoiceCommand {
     public static final Pattern PATTERN = Pattern.compile("^play(?:\\s+(.+))?$", Pattern.CASE_INSENSITIVE);
 
     private final SpotifyClient spotify;
-    private final Optional<String> query;
+    private final String query;
 
     public PlayCommand(SpotifyClient spotify) {
         this.spotify = spotify;
-        this.query = Optional.empty();
+        this.query = null;
     }
 
     public PlayCommand(SpotifyClient spotify, String query) {
         this.spotify = spotify;
-        this.query = Optional.ofNullable(query).filter(s -> !s.isBlank());
+        this.query = (query.isBlank()) ? null : query;
     }
 
     @Override
     public CommandResult execute() throws Exception {
-        if (query.isEmpty()) {
+        if (query == null) {
             spotify.play();
             return new CommandResult(true, "Playing");
-        } else {
-            String q = query.get();
-            Optional<String> uriOpt = spotify.searchTrack(q);
-            if (uriOpt.isPresent()) {
-                spotify.play(uriOpt.get());
-                return new CommandResult(true, "Playing '" + q + "'");
-            } else {
-                return new CommandResult(false, "Song not found: " + q);
-            }
         }
+        Optional<String> uriOpt = spotify.searchTrack(query);
+        if (uriOpt.isPresent()) {
+            spotify.play(uriOpt.get());
+            return new CommandResult(true, "Playing '" + query + "'");
+        }
+        return new CommandResult(false, "Song not found: " + query);
     }
 
     @Override
@@ -58,6 +55,3 @@ public class PlayCommand implements VoiceCommand {
         };
     }
 }
-
-
-
